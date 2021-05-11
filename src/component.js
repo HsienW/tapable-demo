0fb9ddeff648978b6a88a7497e7e1a318b3e5208
Wallace H Wu <wallace_h_wu@wistron.com>
2021-05-11 15:53:14 +0800

    update index test case

--- src/component.js
+++ src/component.js
@@ -1,15 +1,24 @@
-import { SyncHook } from 'tapable';
+import { SyncHook, SyncBailHook } from 'tapable';
 
 // Person 相當於一個 Tapable, 負責 event 的定義跟執行
 export const Person = function () {
-    this.startHook = new SyncHook();
-    this.parameterHook = new SyncHook(['parameter']);
+    this.hooks = {
+        start: new SyncHook(),
+        parameter: new SyncHook(['parameter']),
+        brake: new SyncBailHook()
+    };
 
     this.callHook = function () {
-        this.startHook.call();
+        this.hooks.start.call();
     }
 
     this.callParameterHook = function (parameter) {
-        this.parameterHook.call(parameter);
+        this.hooks.parameter.call(parameter);
+    }
+
+    this.callBrakeHook = function () {
+        this.hooks.brake.call();
     }
 }
+
+
