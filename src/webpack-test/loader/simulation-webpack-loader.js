b2af895eddc975e9a97649d6bb25e0ddbc2a275b
Wallace H Wu <wallace_h_wu@wistron.com>
2021-05-23 12:09:19 +0800

    update

---
+++ src/webpack-test/loader/simulation-webpack-loader.js
@@ -0,0 +1,15 @@
+// const less = require('less');
+const SimulationLoader = function (source, map) {
+    return source.replace(/console\.log\(.*\);?\n/g, '');
+}
+
+// const SimulationAsyncLoader = function (source) {
+//     const callback = this.async();
+//     less.render(source, {sourceMap: {}}, function (err, res) {
+//         let {css, map} = res;
+//         callback(null, css, map);
+//     });
+// }
+
+// module.exports = SimulationAsyncLoader;
+module.exports = SimulationLoader;
