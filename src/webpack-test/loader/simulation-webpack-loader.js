84f56d7a7eb8b14d47ddeea062bce839fac8ebe4
Wallace H Wu <wallace_h_wu@wistron.com>
2021-05-29 17:26:13 +0800

    add test case json

--- src/webpack-test/loader/simulation-webpack-loader.js
+++ src/webpack-test/loader/simulation-webpack-loader.js
@@ -1,8 +1,33 @@
+const {getOptions, parseQuery, stringifyRequest,} = require('loader-utils');
+const {validate} = require('schema-utils');
+const schema = require('./simulation-webpack-loader-schema.json');
+
 // const less = require('less');
 const SimulationLoader = function (source, map) {
     console.log('===== Simulation Loader =====');
     console.log(map);
     console.log(source);
+
+    // 透過 getOptions 取得傳入 loader 的參數, 並轉化成 object 方便操作
+    const options = getOptions(this);
+
+    // parseQuery 可以將 string 類型的參數轉化成 object 方便操作
+    const stringOption = parseQuery('?param1=foo');
+
+    // stringifyRequest 可以將絕對路徑轉化成相對路徑, 避免 import or require 時使用到絕對路徑
+    const changePath = stringifyRequest(this, 'test/component/index.js');
+
+    console.log('===== Simulation schema test =====');
+    const configuration = {test: 'test-option'};
+
+    // validate 不會有回傳, 所以收到 undefined 表示 option 傳入 type 正常, 若是 error 打包時會接噴錯
+    validate(schema, options, configuration);
+    console.log(validate(schema, options, configuration));
+
+    console.log('===== Simulation Loader get options =====');
+    console.log(options);
+    console.log(stringOption);
+    console.log(changePath);
     return source.replace(/console\.log\(.*\);/g, '');
 }
 
